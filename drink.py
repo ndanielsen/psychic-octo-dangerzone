dec31d219059309b05ab0a28aa035d3ffe2c1bfb
Live session user <nathan.danielsen@gmail.com>
2015-05-14 23:26:17 +0000

    Wine consumption report finished

--- drink.py
+++ drink.py
@@ -11,11 +11,29 @@ By Nathan and Sarah
 ###### Import modules
 
 
+import csv
 
 
 
 ###### Input data 
 
+
+countries = []
+wine = []
+
+
+with open('drinks.csv', 'rU') as csvfile:
+	drinksreader = csv.reader(csvfile, delimiter=',')
+	next(drinksreader, None)
+
+	for country_row in drinksreader:
+		countries.append(country_row[0])
+
+		# print country_row[3]
+
+		wine_num = int(country_row[3])
+
+		wine.append(wine_num)
 
 
 ###### Do something cool with the data
@@ -24,6 +42,30 @@ By Nathan and Sarah
 
 
 ###### Report your findings
+
+print "Global Drinking Report: Wine"
+
+print "We looked at this many country:", len(countries) 
+
+print "They are together drank this much wine:", sum(wine)
+
+print "The average wine consumption per country was:", sum(wine) / len(countries)
+
+
+###### Test
+
+#### 'country', 'beer_servings', 'spirit_servings', 'wine_servings', 'total_litres_of_pure_alcohol', 'continent']
+#### ['Zimbabwe', '64', '18', '4', '4.7', 'AF']
+
+
+# print 'hello world!'
+
+
+# print countries[1:3]
+
+# print sum(wine)
+
+
 
 
 
